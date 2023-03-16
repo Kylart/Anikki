09f5be9e5ed138d7ab34855e186aac3e6ca256cb
Kylart <kylart.dev@gmail.com>
2023-03-16 12:42:49 +0100

    Fix default aspect ratio

--- lib/components/custom_grid_view.dart
+++ lib/components/custom_grid_view.dart
@@ -16,7 +16,7 @@ class CustomGridView<T> extends StatelessWidget {
       maxCrossAxisExtent: 300,
       crossAxisSpacing: 8,
       mainAxisSpacing: 8,
-      childAspectRatio: 0.65,
+      childAspectRatio: 0.6,
     ),
   });
 
