19852b10a9decb73e0872bb931e73f24ee953c51
Kylart <kylart.dev@gmail.com>
2023-04-06 22:31:54 +0900

    Added blur to controls background

--- lib/components/video_player/player_controls/player_controls_background.dart
+++ lib/components/video_player/player_controls/player_controls_background.dart
@@ -12,15 +12,20 @@ class PlayerControlsBackground extends StatelessWidget {
         gradient: LinearGradient(
           end: Alignment.topCenter,
           begin: Alignment.bottomCenter,
-          colors: [Colors.black87, Colors.black54],
+          colors: [Colors.black54, Colors.black26],
         ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24.0,
-          vertical: 8.0,
+      child: ClipRRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24.0,
+              vertical: 4.0,
+            ),
+            child: child,
+          ),
         ),
-        child: child,
       ),
     );
   }
