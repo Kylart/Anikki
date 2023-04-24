7bab2cd131986e1c87d976218fdeabeac1353052
Kylart <kylart.dev@gmail.com>
2023-04-24 12:08:55 +0200

    Added some customization for MacOS

--- lib/helpers/desktop_hooks.dart
+++ lib/helpers/desktop_hooks.dart
@@ -4,6 +4,12 @@ import 'package:flutter_acrylic/flutter_acrylic.dart';
 
 Future<void> setUpDesktop() async {
   await Window.initialize();
+
+  if (Platform.isMacOS) {
+    Window.makeTitlebarTransparent();
+    Window.hideTitle();
+    Window.enableFullSizeContentView();
+  }
 }
 
 bool isDesktop() {
