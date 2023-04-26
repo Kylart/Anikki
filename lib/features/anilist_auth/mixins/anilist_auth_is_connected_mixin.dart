37097433dabd738a87dca2550e9888fb67ba1b94
Kylart <kylart.dev@gmail.com>
2023-04-26 18:34:30 +0200

    Cancel stream subscription on dispose

--- lib/features/anilist_auth/mixins/anilist_auth_is_connected_mixin.dart
+++ lib/features/anilist_auth/mixins/anilist_auth_is_connected_mixin.dart
@@ -1,21 +1,33 @@
-import 'package:anikki/features/anilist_auth/bloc/anilist_auth_bloc.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:anikki/features/anilist_auth/bloc/anilist_auth_bloc.dart';
 
 mixin AnilistAuthIsConnectedMixin<T extends StatefulWidget> on State<T> {
   bool isConnected = false;
   AnilistAuthSuccess? auth;
 
+  StreamSubscription<AnilistAuthState>? isConnectedStreamSubscription;
+
   @override
   void initState() {
     super.initState();
 
-    BlocProvider.of<AnilistAuthBloc>(context).stream.listen((state) {
+    isConnectedStreamSubscription =
+        BlocProvider.of<AnilistAuthBloc>(context).stream.listen((state) {
       setState(() {
         isConnected = state is AnilistAuthSuccess;
 
         if (isConnected) auth = state as AnilistAuthSuccess;
       });
     });
+  }
+
+  @override
+  void dispose() {
+    isConnectedStreamSubscription?.cancel();
+    super.dispose();
   }
 }
--- lib/helpers/connectivity_bloc/is_online_mixin.dart
+++ lib/helpers/connectivity_bloc/is_online_mixin.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -6,14 +8,23 @@ import 'package:anikki/helpers/connectivity_bloc/connectivity_bloc.dart';
 mixin IsOnlineMixin<T extends StatefulWidget> on State<T> {
   bool isOnline = false;
 
+  StreamSubscription<ConnectivityState>? onlineSubscriptionSteam;
+
   @override
   void initState() {
     super.initState();
 
-    BlocProvider.of<ConnectivityBloc>(context).stream.listen((state) {
+    onlineSubscriptionSteam =
+        BlocProvider.of<ConnectivityBloc>(context).stream.listen((state) {
       setState(() {
         isOnline = state is ConnectivityOnline;
       });
     });
+  }
+
+  @override
+  void dispose() {
+    onlineSubscriptionSteam?.cancel();
+    super.dispose();
   }
 }
