92441003b8d9efa718b0c295cdf8ea266fb97ae0
Kylart <kylart.dev@gmail.com>
2023-05-04 16:34:17 +0200

    Switch Visibility to Offstage

--- lib/layouts/landscape/custom_app_bar.dart
+++ lib/layouts/landscape/custom_app_bar.dart
@@ -32,38 +32,36 @@ class _CustomAppBarState extends State<CustomAppBar> with IsOnlineMixin {
     return Padding(
       padding: const EdgeInsets.only(top: 12.0, left: 12.0, right: 12.0),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
         children: [
           const Spacer(),
           SizedBox(
             width: 500,
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-              child: TextField(
-                mouseCursor: SystemMouseCursors.click,
-                onTap: onTap,
-                showCursor: false,
-                textAlign: TextAlign.center,
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.search_outlined),
-                  contentPadding:
-                      EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-                  filled: false,
-                  fillColor: Colors.transparent,
-                  focusedBorder: OutlineInputBorder(),
-                  border: OutlineInputBorder(),
-                  hintText: 'Search anything',
-                ),
+            child: TextField(
+              mouseCursor: SystemMouseCursors.click,
+              onTap: onTap,
+              showCursor: false,
+              textAlign: TextAlign.center,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+                filled: false,
+                fillColor: Colors.transparent,
+                focusedBorder: OutlineInputBorder(),
+                border: OutlineInputBorder(),
+                hintText: 'Search anything',
               ),
             ),
           ),
           const Spacer(),
-          if (!isOnline)
-            const Tooltip(
+          Offstage(
+            offstage: isOnline,
+            child: const Tooltip(
               message:
                   'No internet connection detected. Some features will not work.',
               child: Icon(Icons.signal_wifi_connected_no_internet_4),
             ),
+          ),
         ],
       ),
     );
