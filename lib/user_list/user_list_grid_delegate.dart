8082440c22ac6f7128021bb345a9ff331d0d2410
Kylart <kylart.dev@gmail.com>
2023-05-07 20:30:32 +0200

    Revert grid delegate aspect ration

--- lib/user_list/user_list_grid_delegate.dart
+++ lib/user_list/user_list_grid_delegate.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 const userListGridDelegate = SliverGridDelegateWithMaxCrossAxisExtent(
   maxCrossAxisExtent: 220,
-  childAspectRatio: 10 / 15,
+  childAspectRatio: 9 / 15,
 );
