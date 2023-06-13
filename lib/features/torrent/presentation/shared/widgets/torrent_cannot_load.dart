b20003a132f0a84e667d0383699f4d2a92e21719
Kylart <kylart.dev@gmail.com>
2023-06-14 00:50:06 +0200

    Actually hide torrent cannot load widget when dismissed

--- lib/features/torrent/presentation/shared/widgets/torrent_cannot_load.dart
+++ lib/features/torrent/presentation/shared/widgets/torrent_cannot_load.dart
@@ -20,18 +20,21 @@ class _TorrentCannotLoadState extends State<TorrentCannotLoad> {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutCard(
-      child: ListTile(
-        dense: true,
-        title: Text('Could not find ${widget.type.title()} instance'),
-        subtitle: Text(
-          'Are you sure your ${widget.type.title()} is running and accepting connections?',
-        ),
-        trailing: IconButton(
-          onPressed: () => setState(() {
-            dismissed = true;
-          }),
-          icon: const Icon(Icons.close),
+    return Offstage(
+      offstage: dismissed,
+      child: LayoutCard(
+        child: ListTile(
+          dense: true,
+          title: Text('Could not find ${widget.type.title()} instance'),
+          subtitle: Text(
+            'Are you sure your ${widget.type.title()} is running and accepting connections?',
+          ),
+          trailing: IconButton(
+            onPressed: () => setState(() {
+              dismissed = true;
+            }),
+            icon: const Icon(Icons.close),
+          ),
         ),
       ),
     );
