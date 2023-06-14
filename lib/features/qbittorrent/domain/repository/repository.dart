25f41e374cadeaced1c0d46c26f6bf3fa0dc7034
Kylart <kylart.dev@gmail.com>
2023-06-14 18:12:42 +0200

    Added comments to QBitTorrentRepository

--- lib/features/qbittorrent/domain/repository/repository.dart
+++ lib/features/qbittorrent/domain/repository/repository.dart
@@ -10,6 +10,9 @@ enum ApiName {
   torrents,
 }
 
+/// Repository to access a remote QBitTorrent instance. The API is a RPC API
+/// 
+/// API documentation: https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)
 class QBitTorrentRepository {
   QBitTorrentRepository({
     Uri? uri,
@@ -59,6 +62,7 @@ class QBitTorrentRepository {
     }
   }
 
+  /// Internal method to access GET routes of the API
   Future<String> _get(ApiName apiName, String methodName) async {
     assert(
       TorrentsMethod.values.map((e) => e.name).contains(methodName) ||
@@ -76,6 +80,7 @@ class QBitTorrentRepository {
     return utf8.decode(response.bodyBytes);
   }
 
+  /// Internal method to access POST routes of the API
   Future<http.StreamedResponse> _post(
     ApiName apiName,
     String methodName, [
@@ -108,6 +113,10 @@ class QBitTorrentRepository {
     return response;
   }
 
+  /// Login requires usename and password to get a Cookie
+  /// that will be stored internally and used to send requests to the API.
+  /// 
+  /// Doc: https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)#login
   Future<void> login() async {
     final Map<String, String> arguments = hasCredentials
         ? {
@@ -133,6 +142,9 @@ class QBitTorrentRepository {
     }
   }
 
+  /// Returns all the torrents currently in the instance
+  /// 
+  /// Doc: https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)#get-torrent-list
   Future<List<Torrent>> getTorrents() async {
     final response = await _get(ApiName.torrents, TorrentsMethod.info.name);
 
@@ -146,6 +158,9 @@ class QBitTorrentRepository {
     return result;
   }
 
+  /// Pause a torrent. It won't be downloading or seeding.
+  /// 
+  /// Doc: https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)#pause-torrents
   Future<bool> pauseTorrent(String? hash) async {
     await _post(
       ApiName.torrents,
@@ -158,6 +173,9 @@ class QBitTorrentRepository {
     return true;
   }
 
+  /// Resume a paused torrent
+  /// 
+  /// Doc: https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)#resume-torrents
   Future<bool> resumeTorrent(String? hash) async {
     await _post(
       ApiName.torrents,
@@ -170,6 +188,9 @@ class QBitTorrentRepository {
     return true;
   }
 
+  /// Remove a torrent from the instance
+  /// 
+  /// Doc: https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)#delete-torrents
   Future<bool> deleteTorrent(String? hash, [bool deleteFiles = false]) async {
     await _post(
       ApiName.torrents,
@@ -183,6 +204,9 @@ class QBitTorrentRepository {
     return true;
   }
 
+  /// Add a Torrent to the instance
+  /// 
+  /// Doc: https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)#add-new-torrent
   Future<bool> addTorrent(String url,
       {String? path, bool streaming = false}) async {
     await _post(
@@ -199,6 +223,13 @@ class QBitTorrentRepository {
     return true;
   }
 
+  /// Setup a torrent to be 'streamed' without seeking possibilities.
+  /// This will set the right torrent properties to enable sequential 
+  /// download and First-Last pieces download on the torrent.
+  /// 
+  /// Doc:
+  ///   * Sequential DL: https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)#toggle-sequential-download
+  ///   * FL priority: https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)#set-firstlast-piece-priority
   Future<bool> streamTorrent(Torrent torrent) async {
     await Future.wait([
       if (torrent.seqDl != true)
@@ -222,6 +253,16 @@ class QBitTorrentRepository {
     return true;
   }
 
+
+  /// Setup a torrent to be 'streamed' without seeking possibilities.
+  /// This will set the right torrent properties to disable sequential 
+  /// download and First-Last pieces download on the torrent.
+  /// 
+  /// It will NOT remove the torrent from the instance.
+  /// 
+  /// Doc:
+  ///   * Sequential DL: https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)#toggle-sequential-download
+  ///   * FL priority: https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)#set-firstlast-piece-priority
   Future<bool> stopStreamTorrent(Torrent torrent) async {
     await Future.wait([
       if (torrent.seqDl != false)
