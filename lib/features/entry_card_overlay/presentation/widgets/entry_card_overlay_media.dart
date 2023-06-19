b95f5c3b21e0cf49f2bb991b4971a89fa03282fc
Kylart <kylart.dev@gmail.com>
2023-06-20 00:36:48 +0200

    Removed backdrop filter for a LayoutCard

--- lib/features/entry_card_overlay/presentation/widgets/entry_card_overlay_media.dart
+++ lib/features/entry_card_overlay/presentation/widgets/entry_card_overlay_media.dart
@@ -1,8 +1,5 @@
 import 'dart:math';
-import 'dart:ui';
 
-import 'package:anikki/features/entry_card_overlay/presentation/widgets/entry_card_overlay_episodes.dart';
-import 'package:anikki/features/library/presentation/bloc/library_bloc.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/cupertino.dart';
@@ -11,6 +8,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:anikki/core/core.dart';
 import 'package:anikki/core/widgets/entry/entry_tag.dart';
+import 'package:anikki/core/widgets/layout_card.dart';
+import 'package:anikki/features/entry_card_overlay/presentation/widgets/entry_card_overlay_episodes.dart';
+import 'package:anikki/features/library/presentation/bloc/library_bloc.dart';
 import 'package:anikki/features/entry_card_overlay/presentation/widgets/entry_card_overlay_actions.dart';
 import 'package:anikki/features/entry_card_overlay/presentation/widgets/entry_card_overlay_trailer.dart';
 import 'package:anikki/features/entry_card_overlay/presentation/bloc/entry_card_overlay_bloc.dart';
@@ -40,102 +40,93 @@ class EntryCardOverlayMedia extends StatelessWidget {
             builder: (context, state) {
           final isExpanded = (state as EntryCardOverlayActive).isExpanded;
 
-          return Card(
-            child: ClipRRect(
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    bottom: 8.0,
-                  ),
-                  child: Column(
-                    children: [
-                      ListTile(
-                        title: AutoSizeText(
-                          media.title ?? 'N/A',
-                          maxLines: 2,
-                        ),
-                        subtitle: media.anilistInfo.title?.native == null
-                            ? const SizedBox()
-                            : Text(media.anilistInfo.title!.native!),
-                        trailing: EntryTag(
-                          padding: EdgeInsets.zero,
-                          child: isExpanded
-                              ? IconButton(
-                                  onPressed: () {
-                                    BlocProvider.of<EntryCardOverlayBloc>(
-                                            context)
-                                        .add(
-                                      EntryCardOverlayClosed(),
-                                    );
-                                  },
-                                  icon: const Icon(Icons.close),
-                                )
-                              : IconButton(
-                                  onPressed: () {
-                                    BlocProvider.of<EntryCardOverlayBloc>(
-                                            context)
-                                        .add(
-                                      EntryCardOverlayExpanded(
-                                          context: context),
-                                    );
-                                  },
-                                  icon: Transform(
-                                    alignment: Alignment.center,
-                                    transform: Matrix4.rotationY(pi),
-                                    child: const Icon(
-                                      CupertinoIcons
-                                          .arrow_up_left_arrow_down_right,
-                                    ),
-                                  ),
+          return LayoutCard(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                bottom: 8.0,
+              ),
+              child: Column(
+                children: [
+                  ListTile(
+                    title: AutoSizeText(
+                      media.title ?? 'N/A',
+                      maxLines: 2,
+                    ),
+                    subtitle: media.anilistInfo.title?.native == null
+                        ? const SizedBox()
+                        : Text(media.anilistInfo.title!.native!),
+                    trailing: EntryTag(
+                      padding: EdgeInsets.zero,
+                      child: isExpanded
+                          ? IconButton(
+                              onPressed: () {
+                                BlocProvider.of<EntryCardOverlayBloc>(context)
+                                    .add(
+                                  EntryCardOverlayClosed(),
+                                );
+                              },
+                              icon: const Icon(Icons.close),
+                            )
+                          : IconButton(
+                              onPressed: () {
+                                BlocProvider.of<EntryCardOverlayBloc>(context)
+                                    .add(
+                                  EntryCardOverlayExpanded(context: context),
+                                );
+                              },
+                              icon: Transform(
+                                alignment: Alignment.center,
+                                transform: Matrix4.rotationY(pi),
+                                child: const Icon(
+                                  CupertinoIcons.arrow_up_left_arrow_down_right,
                                 ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: EntryCardOverlayActions(
-                          media: media.anilistInfo,
-                          entry: libraryEntry,
-                        ),
-                      ),
-                      if (media.anilistInfo.trailer?.id != null &&
-                          media.anilistInfo.trailer?.site == 'youtube' &&
-                          media.anilistInfo.trailer?.thumbnail != null)
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: Center(
-                              child: EntryCardOverlayTrailer(media: media),
+                              ),
                             ),
-                          ),
-                        )
-                      else
-                        const Spacer(),
-                      if (media.anilistInfo.genres != null &&
-                          media.anilistInfo.genres!.isNotEmpty)
-                        Wrap(
-                          alignment: WrapAlignment.center,
-                          runSpacing: 4.0,
-                          spacing: 4.0,
-                          children: media.anilistInfo.genres!.map(
-                            (genre) {
-                              return EntryTag(
-                                child: Text(
-                                  genre.toString(),
-                                  style: const TextStyle(fontSize: 10.0),
-                                ),
-                              );
-                            },
-                          ).toList(),
-                        ),
-                      if (isExpanded)
-                        EntryCardOverlayEpisodes(
-                          media: media,
-                          entry: libraryEntry,
-                        ),
-                    ],
+                    ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: EntryCardOverlayActions(
+                      media: media.anilistInfo,
+                      entry: libraryEntry,
+                    ),
+                  ),
+                  if (media.anilistInfo.trailer?.id != null &&
+                      media.anilistInfo.trailer?.site == 'youtube' &&
+                      media.anilistInfo.trailer?.thumbnail != null)
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Center(
+                          child: EntryCardOverlayTrailer(media: media),
+                        ),
+                      ),
+                    )
+                  else
+                    const Spacer(),
+                  if (media.anilistInfo.genres != null &&
+                      media.anilistInfo.genres!.isNotEmpty)
+                    Wrap(
+                      alignment: WrapAlignment.center,
+                      runSpacing: 4.0,
+                      spacing: 4.0,
+                      children: media.anilistInfo.genres!.map(
+                        (genre) {
+                          return EntryTag(
+                            child: Text(
+                              genre.toString(),
+                              style: const TextStyle(fontSize: 10.0),
+                            ),
+                          );
+                        },
+                      ).toList(),
+                    ),
+                  if (isExpanded)
+                    EntryCardOverlayEpisodes(
+                      media: media,
+                      entry: libraryEntry,
+                    ),
+                ],
               ),
             ),
           );
