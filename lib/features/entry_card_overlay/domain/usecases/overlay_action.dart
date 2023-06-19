6a894cedde70bc1844a7b64d0344dda47ffca089
Kylart <kylart.dev@gmail.com>
2023-06-20 00:42:07 +0200

    Do not force hide the overlay on overlay action

--- lib/features/entry_card_overlay/domain/usecases/overlay_action.dart
+++ lib/features/entry_card_overlay/domain/usecases/overlay_action.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'package:anikki/features/entry_card_overlay/presentation/bloc/entry_card_overlay_bloc.dart';
 
 void overlayAction(void Function() callback, BuildContext context) {
   callback();
-  BlocProvider.of<EntryCardOverlayBloc>(context).add(EntryCardOverlayClosed());
 }
