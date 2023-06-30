d1fbede650ec3e0524cc4a540545b137243b061b
Kylart <kylart.dev@gmail.com>
2023-07-01 01:52:21 +0200

    Redesigned search view to use LayoutCard

--- lib/features/search/presentation/view/search_view.dart
+++ lib/features/search/presentation/view/search_view.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:anikki/core/widgets/error_tile.dart';
+import 'package:anikki/core/widgets/layout_card.dart';
+import 'package:anikki/features/layouts/presentation/bloc/layout_bloc.dart';
 import 'package:anikki/features/search/presentation/widgets/anikki_search_bar.dart';
 import 'package:anikki/features/search/presentation/bloc/search_bloc.dart';
 import 'package:anikki/features/search/presentation/widgets/search_results.dart';
-import 'package:anikki/features/layouts/shared/helpers/helpers.dart';
 import 'package:anikki/core/widgets/loader.dart';
 
 class SearchView extends StatefulWidget {
@@ -21,53 +22,74 @@ class _SearchViewState extends State<SearchView> {
   Widget build(BuildContext context) {
     return BlocBuilder<SearchBloc, SearchState>(
       builder: (context, state) {
-        final landscape = isLandscape(context);
-
-        return SizedBox(
-          width: landscape ? 800 : null,
-          child: Card(
-            surfaceTintColor:
-                state.term.isEmpty || !landscape ? Colors.transparent : null,
-            color: state.term.isEmpty || !landscape ? Colors.transparent : null,
-            shape: landscape
-                ? null
-                : RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(0.0),
-                  ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const AnikkiSearchBar(),
-                  if (state is! SearchEmptyTerm) const Divider(),
-                  if (state is SearchLoading)
-                    const Expanded(
-                      child: Loader(),
-                    ),
-                  if (state is SearchSuccess)
-                    Expanded(
-                      child: state.isEmpty
-                          ? SizedBox(
-                              width: 300,
-                              child: EmptyWidget(
-                                title: 'Could not find anything...',
-                              ),
-                            )
-                          : SearchResults(
-                              state: state,
-                            ),
-                    ),
-                  if (state is SearchError)
-                    ErrorTile(
-                      description: state.message,
-                    )
-                ],
-              ),
-            ),
+        return SearchViewContainer(
+          isEmpty:
+              state is SearchSuccess ? state.isEmpty : state is SearchEmptyTerm,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const AnikkiSearchBar(),
+              if (state is! SearchEmptyTerm) const Divider(),
+              if (state is SearchLoading)
+                const Expanded(
+                  child: Loader(),
+                ),
+              if (state is SearchSuccess)
+                Expanded(
+                  child: state.isEmpty
+                      ? SizedBox(
+                          width: 300,
+                          child: EmptyWidget(
+                            title: 'Could not find anything...',
+                          ),
+                        )
+                      : SearchResults(
+                          state: state,
+                        ),
+                ),
+              if (state is SearchError)
+                ErrorTile(
+                  description: state.message,
+                ),
+            ],
           ),
         );
       },
     );
+  }
+}
+
+class SearchViewContainer extends StatelessWidget {
+  const SearchViewContainer({
+    super.key,
+    required this.child,
+    this.isEmpty = true,
+  });
+
+  final bool isEmpty;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LayoutBloc, LayoutState>(builder: (context, state) {
+      final landscape = state is LayoutLandscape;
+      if (landscape) {
+        return LayoutCard(
+          transparent: isEmpty,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: SizedBox(
+              width: 800,
+              child: child,
+            ),
+          ),
+        );
+      } else {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: child,
+        );
+      }
+    });
   }
 }
