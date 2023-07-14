d4b309c91ac689efec670c4a26b6f18d98efe5cb
Kylart <kylart.dev@gmail.com>
2023-07-14 13:59:57 +0200

    Updated drag controls

--- lib/features/video_player/presentation/widgets/player_controls/player_controls.dart
+++ lib/features/video_player/presentation/widgets/player_controls/player_controls.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/material.dart';
--- lib/features/video_player/presentation/widgets/player_controls/player_cursor_handler.dart
+++ lib/features/video_player/presentation/widgets/player_controls/player_cursor_handler.dart
@@ -1,6 +1,6 @@
 part of 'player_controls.dart';
 
-class PlayerCursorHandler extends StatelessWidget {
+class PlayerCursorHandler extends StatefulWidget {
   const PlayerCursorHandler({
     super.key,
     required this.player,
@@ -9,6 +9,42 @@ class PlayerCursorHandler extends StatelessWidget {
 
   final Player player;
   final Widget child;
+
+  @override
+  State<PlayerCursorHandler> createState() => _PlayerCursorHandlerState();
+}
+
+class _PlayerCursorHandlerState extends State<PlayerCursorHandler> {
+  bool showVolumeBar = false;
+  bool showBrightnessBar = false;
+
+  bool showSeekNumber = false;
+
+  /// Drag is only available on mobile. However, a drag event should be ignored
+  /// when starting on the very side of the device as to not act on the video
+  /// when the user is using native gestures.
+  ///
+  /// A drag event should be ignore when not in the `kDragIgnorePercent` of the
+  /// device.
+  bool _shouldIgnoreDrag(BuildContext context, DragUpdateDetails details) {
+    if (isDesktop()) return true;
+
+    final screenSize = MediaQuery.of(context).size;
+    final position = details.globalPosition;
+
+    return !(
+        // Height top
+        position.dy < screenSize.height - 20 &&
+
+            // Height bottom
+            position.dy > 20 &&
+
+            // Width right
+            position.dx < screenSize.width - 60 &&
+
+            // Width left
+            position.dx > 60);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +57,7 @@ class PlayerCursorHandler extends StatelessWidget {
       onLongPress: () {
         if (desktop) return;
 
-        player.playOrPause();
+        widget.player.playOrPause();
         videoBloc.add(VideoPlayerResetShowTimer());
       },
       onDoubleTapDown: (details) {
@@ -33,19 +69,46 @@ class PlayerCursorHandler extends StatelessWidget {
         final screenWidth = MediaQuery.of(context).size.width;
 
         if (details.globalPosition.dx < screenWidth / 2) {
-          player.seek(
-            player.state.position + const Duration(seconds: -10),
+          widget.player.seek(
+            widget.player.state.position + const Duration(seconds: -10),
           );
         } else {
-          player.seek(
-            player.state.position + const Duration(seconds: 10),
+          widget.player.seek(
+            widget.player.state.position + const Duration(seconds: 10),
           );
         }
       },
       onHorizontalDragUpdate: (details) {
-        player.seek(
-          player.state.position + Duration(seconds: (details.delta.dx.toInt())),
+        if (_shouldIgnoreDrag(context, details)) return;
+
+        final delta = details.primaryDelta;
+
+        if (delta == null) return;
+
+        var value = delta.ceilToDouble() * 2;
+
+        if (value.abs() < 5) return;
+
+        widget.player.seek(
+          widget.player.state.position +
+              Duration(
+                seconds: value.toInt(),
+              ),
         );
+      },
+      onVerticalDragUpdate: (details) {
+        if (_shouldIgnoreDrag(context, details)) return;
+
+        final currentVolume = widget.player.state.volume;
+        final delta = details.primaryDelta?.ceilToDouble() ?? 0;
+
+        final newVolume = currentVolume - delta;
+
+        if (delta.sign < 0) {
+          widget.player.setVolume(min(newVolume, 100.0));
+        } else {
+          widget.player.setVolume(max(newVolume, 0.0));
+        }
       },
       child: MouseRegion(
         cursor:
@@ -58,7 +121,7 @@ class PlayerCursorHandler extends StatelessWidget {
           child: AnimatedOpacity(
             duration: const Duration(milliseconds: 300),
             opacity: hideControls ? 0.0 : 1.0,
-            child: child,
+            child: widget.child,
           ),
         ),
       ),
