ce61f9154299df3b2770eef32a9e829b75684658
Kylart <kylart.dev@gmail.com>
2023-12-13 02:43:47 +0100

    Added automatic background media change on mobile

--- lib/app/home/view/home_view.dart
+++ lib/app/home/view/home_view.dart
@@ -1,18 +1,79 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:anikki/app/anilist_auth/bloc/anilist_auth_bloc.dart';
+import 'package:anikki/app/home/bloc/home_bloc.dart';
 import 'package:anikki/app/home/widgets/animated_media_banner.dart';
-import 'package:anikki/app/home/shared/widgets/custom_menu.dart';
 import 'package:anikki/app/home/widgets/home_search_bar.dart';
 import 'package:anikki/app/home_continue/home_continue.dart';
+import 'package:anikki/app/home_continue/bloc/home_continue_bloc.dart';
 import 'package:anikki/app/home_feed/home_feed.dart';
+import 'package:anikki/app/home_feed/bloc/home_feed_bloc.dart';
 import 'package:anikki/app/home_more/home_more.dart';
 import 'package:anikki/app/home_start/home_start.dart';
+import 'package:anikki/app/home_start/bloc/home_start_bloc.dart';
+import 'package:anikki/app/home/shared/widgets/custom_menu.dart';
 import 'package:anikki/app/layouts/bloc/layout_bloc.dart';
+import 'package:anikki/core/core.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  Timer? interval;
+
+  void updateBackgroundMedia() {
+    final homeBloc = BlocProvider.of<HomeBloc>(context);
+    final entries = <Media>[
+      ...BlocProvider.of<HomeContinueBloc>(context)
+          .state
+          .entries
+          .map((e) => Media(anilistInfo: e.media)),
+      ...BlocProvider.of<HomeStartBloc>(context)
+          .state
+          .entries
+          .map((e) => Media(anilistInfo: e.media)),
+      ...BlocProvider.of<HomeFeedBloc>(context).state.entries,
+    ].where(
+      (element) => element.bannerImage != null,
+    );
+
+    if (entries.isEmpty) return;
+
+    homeBloc.add(
+      HomeMediaChanged(
+        entries.elementAt(
+          Random().nextInt(entries.length),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (Platform.isIOS || Platform.isAndroid) {
+      interval = Timer.periodic(
+        const Duration(seconds: 10),
+        (timer) => updateBackgroundMedia(),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    interval?.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
