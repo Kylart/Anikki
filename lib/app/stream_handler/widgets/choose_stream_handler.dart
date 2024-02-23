f0376120545dee34d74d550fcd0f90abe0b7b1be
Kylart <kylart.dev@gmail.com>
2024-02-24 00:24:48 +0100

    Removed svg icons for font icons

--- lib/app/stream_handler/widgets/choose_stream_handler.dart
+++ lib/app/stream_handler/widgets/choose_stream_handler.dart
@@ -2,12 +2,12 @@ import 'package:anikki/app/downloader/bloc/downloader_bloc.dart';
 import 'package:anikki/app/settings/bloc/settings_bloc.dart';
 import 'package:anikki/app/stream_handler/widgets/choose_stream_button.dart';
 import 'package:anikki/core/core.dart';
+import 'package:anikki/core/widgets/custom_app_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:anikki/app/stream_handler/bloc/stream_handler_bloc.dart';
 import 'package:anikki/core/widgets/layout_card.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ionicons/ionicons.dart';
 
 class ChooseStreamHandler extends StatelessWidget {
@@ -62,15 +62,15 @@ class ChooseStreamHandler extends StatelessWidget {
                     child: BlocBuilder<SettingsBloc, SettingsState>(
                       builder: (context, state) =>
                           switch (state.settings.torrentType) {
-                        TorrentType.transmission => SvgPicture.asset(
-                            'assets/icons/transmission.svg',
-                            height: 72,
-                            color: const Color(0xFFD70008),
+                        TorrentType.transmission => const Icon(
+                            CustomIcons.transmission,
+                            size: 72,
+                            color: Color(0xFFD70008),
                           ),
-                        TorrentType.qbittorrent => SvgPicture.asset(
-                            'assets/icons/qbittorrent.svg',
-                            height: 72,
-                            color: const Color(0xFF2F67BA),
+                        TorrentType.qbittorrent => const Icon(
+                            CustomIcons.qbittorrent,
+                            size: 72,
+                            color: Color(0xFF2F67BA),
                           ),
                         TorrentType.none => throw UnimplementedError(),
                       },
---
+++ lib/core/widgets/custom_app_icons.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const _kFontFam = 'CustomIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData transmission =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData qbittorrent =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
