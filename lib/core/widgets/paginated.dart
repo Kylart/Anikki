9d80d2920f4e1a6fc0aef9123c88d7b5252b5e76
Kylart <kylart.dev@gmail.com>
2024-03-23 00:00:41 +0100

    Fixed padding for pagination

--- lib/core/widgets/paginated.dart
+++ lib/core/widgets/paginated.dart
@@ -50,7 +50,11 @@ class _PaginatedState extends State<Paginated> {
       children: [
         if (maxPage != 0)
           Padding(
-            padding: const EdgeInsets.all(4.0),
+            padding: const EdgeInsets.only(
+              left: 4.0,
+              right: 4.0,
+              bottom: 8.0,
+            ),
             child: SizedBox(
               height: 40,
               child: Padding(
