f0cff6640d85912a2fe05e6402a43883f90c687f
Kylart <kylart.dev@gmail.com>
2024-08-16 00:34:13 +0200

    Set up background for HomeCarousel

--- lib/app/home/shared/widgets/home_carousel.dart/home_carousel.dart
+++ lib/app/home/shared/widgets/home_carousel.dart/home_carousel.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
@@ -10,8 +11,8 @@ import 'package:super_sliver_list/super_sliver_list.dart';
 import 'package:anikki/app/home/bloc/home_bloc.dart';
 import 'package:anikki/core/core.dart';
 
-part 'home_carousel_title.dart';
 part 'home_carousel_navigation.dart';
+part 'home_carousel_title.dart';
 
 class HomeCarousel extends StatefulWidget {
   const HomeCarousel({
@@ -102,6 +103,8 @@ class _HomeCarouselState extends State<HomeCarousel> {
       scrollController: scrollController,
       alignment: 0.0,
     );
+
+    updateCurrentMedia();
   }
 
   @override
@@ -115,95 +118,126 @@ class _HomeCarouselState extends State<HomeCarousel> {
       bottom: 0,
       width: width,
       height: height,
-      child: Stack(
-        children: [
-          Positioned.fill(
-            child: SuperListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              listController: listController,
-              controller: scrollController,
-              itemCount: 10000000,
-              itemBuilder: (context, index) {
-                final i = index % widget.medias.length;
-                final media = widget.medias.elementAt(i);
-
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: AnimatedContainer(
-                      duration: itemAnimationDuration,
-                      constraints: BoxConstraints(
-                        maxHeight:
-                            index == currentIndex ? height : height / 1.5,
-                      ),
-                      decoration: BoxDecoration(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(8.0)),
-                        image: DecorationImage(
-                          fit: BoxFit.fill,
-                          image: NetworkImage(
-                            media.coverImage ?? '',
-                          ),
-                        ),
-                      ),
-                      child: AspectRatio(
-                        aspectRatio: itemAspectRatio,
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-          Positioned(
-            top: 0,
-            left: height * itemAspectRatio + 12,
-            width: width - (height * itemAspectRatio + 12),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Flexible(
-                        child: _HomeCarouselTitle(
-                          currentMedia: currentMedia,
-                        ),
-                      ),
-                      _HomeCarouselNavigation(
-                        text:
-                            '${currentMediaIndex + 1} / ${widget.medias.length}',
-                        onNext: () => goToItem(
-                          currentIndex + 1,
-                          resetTimer: true,
-                        ),
-                        onPrevious: () => goToItem(
-                          currentIndex - 1,
-                          resetTimer: true,
-                        ),
-                      ),
-                    ],
-                  ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(8.0),
+          bottomLeft: Radius.circular(8.0),
+        ),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  context.colorScheme.surface.withOpacity(0.2),
+                  context.colorScheme.surface.withOpacity(0.1),
                 ],
               ),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(8.0),
+                bottomLeft: Radius.circular(8.0),
+              ),
+              border: Border.all(
+                color: context.colorScheme.outline.withOpacity(0.1),
+              ),
             ),
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: SuperListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    listController: listController,
+                    controller: scrollController,
+                    itemCount: 10000000,
+                    itemBuilder: (context, index) {
+                      final i = index % widget.medias.length;
+                      final media = widget.medias.elementAt(i);
+
+                      return Padding(
+                        padding: index == currentIndex
+                            ? const EdgeInsets.only(right: 12.0)
+                            : const EdgeInsets.symmetric(horizontal: 12.0),
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: AnimatedContainer(
+                            duration: itemAnimationDuration,
+                            constraints: BoxConstraints(
+                              maxHeight:
+                                  index == currentIndex ? height : height / 1.5,
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(8.0)),
+                              image: DecorationImage(
+                                fit: BoxFit.fill,
+                                image: NetworkImage(
+                                  media.coverImage ?? '',
+                                ),
+                              ),
+                            ),
+                            child: AspectRatio(
+                              aspectRatio: itemAspectRatio,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  left: height * itemAspectRatio + 12,
+                  width: width - (height * itemAspectRatio + 12),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24.0, vertical: 4.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Flexible(
+                              child: _HomeCarouselTitle(
+                                currentMedia: currentMedia,
+                              ),
+                            ),
+                            _HomeCarouselNavigation(
+                              text:
+                                  '${currentMediaIndex + 1} / ${widget.medias.length}',
+                              onNext: () => goToItem(
+                                currentIndex + 1,
+                                resetTimer: true,
+                              ),
+                              onPrevious: () => goToItem(
+                                currentIndex - 1,
+                                resetTimer: true,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )
+                .animate()
+                .fadeIn(
+                  duration: 500.ms,
+                )
+                .slideX(
+                  duration: 500.ms,
+                  end: 0,
+                  begin: 0.5,
+                ),
           ),
-        ],
-      )
-          .animate()
-          .fadeIn(
-            duration: 500.ms,
-          )
-          .slideX(
-            duration: 500.ms,
-            end: 0,
-            begin: 0.5,
-          ),
+        ),
+      ),
     );
   }
 }
