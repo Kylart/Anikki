7139fe47118c2c10412c6170d7183a6d70296bb8
Kylart <kylart.dev@gmail.com>
2024-08-20 18:53:01 +0200

    Fix settings page background color

--- lib/app/settings/view/settings_view.dart
+++ lib/app/settings/view/settings_view.dart
@@ -1,4 +1,3 @@
-import 'package:anikki/core/widgets/section/section_title.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:settings_ui/settings_ui.dart';
@@ -6,6 +5,8 @@ import 'package:settings_ui/settings_ui.dart';
 import 'package:anikki/app/anilist_auth/bloc/anilist_auth_bloc.dart';
 import 'package:anikki/app/layouts/bloc/layout_bloc.dart';
 import 'package:anikki/app/settings/widgets/sections/sections.dart';
+import 'package:anikki/core/core.dart';
+import 'package:anikki/core/widgets/section/section_title.dart';
 
 class SettingsView extends StatefulWidget {
   const SettingsView({super.key});
@@ -40,6 +41,16 @@ class _SettingsViewState extends State<SettingsView> {
             ],
             Expanded(
               child: SettingsList(
+                darkTheme: SettingsThemeData(
+                  settingsListBackground: context.colorScheme.surface,
+                  settingsSectionBackground:
+                      context.colorScheme.surfaceContainerHighest,
+                ),
+                lightTheme: SettingsThemeData(
+                  settingsListBackground: context.colorScheme.surface,
+                  settingsSectionBackground:
+                      context.colorScheme.surfaceContainerHighest,
+                ),
                 sections: [
                   const GeneralSection(),
                   if (anilistAuthBloc.isConnected) const AnilistSection(),
