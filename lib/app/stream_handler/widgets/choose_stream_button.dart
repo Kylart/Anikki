db5422401e22258ad4f91d3de1c3fffb752a2ecb
Kylart <kylart.dev@gmail.com>
2024-09-23 00:16:25 +0200

    Updated choose stream handler layout

--- lib/app/stream_handler/widgets/choose_stream_button.dart
+++ lib/app/stream_handler/widgets/choose_stream_button.dart
@@ -14,31 +14,25 @@ class ChooseStreamButton extends StatelessWidget {
   /// Will be used as tooltip message. If none is given, there won't be any tooltip.
   final String? title;
 
-  final borderRadius = const BorderRadius.all(Radius.circular(20.0));
+  final borderRadius = const BorderRadius.all(Radius.circular(12.0));
 
   @override
   Widget build(BuildContext context) {
-    return TooltipVisibility(
-      visible: title != null,
-      child: Tooltip(
-        message: title ?? '',
-        verticalOffset: 60,
-        child: Container(
-          decoration: BoxDecoration(
+    return Tooltip(
+      message: title ?? '',
+      verticalOffset: 60,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: borderRadius,
+        ),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
             borderRadius: borderRadius,
-            border: Border.all(
-              color: Theme.of(context).colorScheme.outline,
-            ),
-          ),
-          child: Material(
-            color: Colors.transparent,
-            child: InkWell(
-              borderRadius: borderRadius,
-              onTap: onTap,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: child,
-              ),
+            onTap: onTap,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: child,
             ),
           ),
         ),
--- lib/app/stream_handler/widgets/choose_stream_handler.dart
+++ lib/app/stream_handler/widgets/choose_stream_handler.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:ionicons/ionicons.dart';
+import 'package:hugeicons/hugeicons.dart';
 
 import 'package:anikki/app/downloader/bloc/downloader_bloc.dart';
 import 'package:anikki/app/settings/bloc/settings_bloc.dart';
@@ -8,7 +8,6 @@ import 'package:anikki/app/stream_handler/bloc/stream_handler_bloc.dart';
 import 'package:anikki/app/stream_handler/widgets/choose_stream_button.dart';
 import 'package:anikki/core/core.dart';
 import 'package:anikki/core/widgets/custom_app_icons.dart';
-import 'package:anikki/core/widgets/layout_card.dart';
 
 class ChooseStreamHandler extends StatelessWidget {
   const ChooseStreamHandler({
@@ -31,73 +30,65 @@ class ChooseStreamHandler extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutCard(
-      child: SizedBox(
-        width: 200,
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'Choose a streaming method',
-                style: Theme.of(context).textTheme.titleMedium,
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      decoration: BoxDecoration(
+        color: context.colorScheme.surface,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(12.0),
+        ),
+      ),
+      child: BlocBuilder<SettingsBloc, SettingsState>(
+        builder: (context, settingsState) => Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ChooseStreamButton(
+              title: settingsState.settings.torrentType.title,
+              onTap: () {
+                BlocProvider.of<DownloaderBloc>(context).add(
+                  DownloaderRequested(
+                    media: state.media,
+                    entry: state.entry,
+                    episode: state.minEpisode,
+                    isStreaming: true,
+                  ),
+                );
+                close(context);
+              },
+              child: switch (settingsState.settings.torrentType) {
+                TorrentType.transmission => const Icon(
+                    CustomIcons.transmission,
+                    size: 72,
+                    color: Color(0xFFD70008),
+                  ),
+                TorrentType.qbittorrent => const Icon(
+                    CustomIcons.qbittorrent,
+                    size: 72,
+                    color: Color(0xFF2F67BA),
+                  ),
+                TorrentType.none => const SizedBox(),
+              },
+            ),
+            ChooseStreamButton(
+              title: 'Web hosted',
+              onTap: () {
+                close(context);
+                BlocProvider.of<StreamHandlerBloc>(context).add(
+                  StreamHandlerShowRequested(
+                    media: state.media,
+                    minEpisode: state.minEpisode,
+                    type: StreamRequestType.online,
+                  ),
+                );
+              },
+              child: const Icon(
+                HugeIcons.strokeRoundedGlobalSearch,
+                size: 72,
+                color: Color.fromARGB(255, 0, 210, 0),
               ),
-              const Divider(),
-              BlocBuilder<SettingsBloc, SettingsState>(
-                builder: (context, settingsState) => Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ChooseStreamButton(
-                      title: settingsState.settings.torrentType.title,
-                      onTap: () {
-                        BlocProvider.of<DownloaderBloc>(context).add(
-                          DownloaderRequested(
-                            media: state.media,
-                            entry: state.entry,
-                            episode: state.minEpisode,
-                            isStreaming: true,
-                          ),
-                        );
-                        close(context);
-                      },
-                      child: switch (settingsState.settings.torrentType) {
-                        TorrentType.transmission => const Icon(
-                            CustomIcons.transmission,
-                            size: 72,
-                            color: Color(0xFFD70008),
-                          ),
-                        TorrentType.qbittorrent => const Icon(
-                            CustomIcons.qbittorrent,
-                            size: 72,
-                            color: Color(0xFF2F67BA),
-                          ),
-                        TorrentType.none => throw UnimplementedError(),
-                      },
-                    ),
-                    ChooseStreamButton(
-                      title: 'Web hosted',
-                      onTap: () {
-                        close(context);
-                        BlocProvider.of<StreamHandlerBloc>(context).add(
-                          StreamHandlerShowRequested(
-                            media: state.media,
-                            minEpisode: state.minEpisode,
-                            type: StreamRequestType.online,
-                          ),
-                        );
-                      },
-                      child: const Icon(
-                        Ionicons.globe_outline,
-                        size: 72,
-                        color: Color.fromARGB(255, 0, 210, 0),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
